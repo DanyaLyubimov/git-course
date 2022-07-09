7cf92edc088a5b9c925c5f0ac7e9af42b135d284
DanyaLyubimov <danya.lubimov@yandex.ru>
2022-07-09 11:36:03 +0300

    Two println

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, branch2");
+        System.out.println("Hello, branch2");
         Thread myThread = new Thread(new Runnable() {
             @Override
             public void run() {
