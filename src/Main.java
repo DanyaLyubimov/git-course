bf105702284c1876f977ff5a5ecd80cc8f3320a3
Danya <danya.lubimov@yandex.ru>
2022-07-09 10:53:47 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello");
         Thread myThread = new Thread(new Runnable() {
             @Override
             public void run() {
